b3ed60780c125beccf9af7a725d7bea500476188
mmamorim <marc.m.amorim@gmail.com>
2022-10-10 20:17:16 -0300

    versão 2

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ import bodyParser from 'body-parser'
 await sgdb.init()
 
 const app = express()
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+    extended: true
 }));
 
 app.get('/', function (req, res) {
@@ -23,7 +23,7 @@ app.get('/jogos/:id', function (req, res) {
     console.log('alguém fez requisição GET /jogos/:id');
     console.log(req.params);
     let jogo = sgdb.db.jogos[req.params.id]
-    if(jogo == undefined) {
+    if (jogo == undefined) {
         res.json({})
     }
     res.json(jogo)
@@ -38,7 +38,7 @@ app.get('/jogadores/:id', function (req, res) {
     console.log('alguém fez requisição GET /jogadores/:id');
     console.log(req.params);
     let jogador = sgdb.db.jogadores[req.params.id]
-    if(jogador == undefined) {
+    if (jogador == undefined) {
         res.json({})
     }
     res.json(jogador)
@@ -46,10 +46,44 @@ app.get('/jogadores/:id', function (req, res) {
 
 app.post('/jogos', function (req, res) {
     console.log('alguém fez requisição POST /jogos');
-    console.log(req.body);
-    sgdb.db.jogos[req.body.id] = req.body
-    sgdb.write()
-    res.send('ok')
+    console.log('conteúdo do body:', req.body);
+    if (sgdb.db.jogos[req.body.id] != undefined) {
+        res.status(400).send('ERRO: ID já existe!')
+    } else {
+        sgdb.db.jogos[req.body.id] = req.body
+        sgdb.write()
+        res.status(200).json(req.body)
+    }
+})
+
+app.put('/jogos/:id', function (req, res) {
+    console.log('alguém fez requisição PUT /jogos');
+    console.log('conteúdo do body:', req.body);
+    console.log('id recebido como parâmetro:', req.params.id);
+    if (sgdb.db.jogos[req.params.id] == undefined) {
+        res.status(400).send('ERRO: ID não existe!')
+    } else {
+        if (req.body.id != req.params.id) {
+            res.status(400).send('ERRO: ID diferentes!')
+        } else {
+            sgdb.db.jogos[req.params.id] = req.body
+            sgdb.write()
+            res.status(200).json(req.body)
+        }
+    }
+})
+
+app.delete('/jogos/:id', function (req, res) {
+    console.log('alguém fez requisição DELETE /jogos');
+    console.log('id recebido como parâmetro:', req.params.id);
+    if (sgdb.db.jogos[req.params.id] == undefined) {
+        res.status(400).send('ERRO: ID não existe!')
+    } else {
+        let obj = sgdb.db.jogos[req.params.id] 
+        delete sgdb.db.jogos[req.params.id]
+        sgdb.write()
+        res.status(200).json(obj)
+    }
 })
 
 
